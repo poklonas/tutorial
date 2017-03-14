46b84fa7e6c727b3f045e2c90179dee855fcd930
Sarik Kumpan <a5810164@gmail.com>
2017-03-14 07:36:47 +0700

    Step 4: Add new User and show all User in index page and each user can show detail when click to there page

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'ledger'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^/add_user_page/$', views.add_user_page, name='add_user_page'),
+    url(r'^/add_user/$', views.add_user, name='add_user'),
+    url(r'^(?P<pk>[0-9]+)/user_detail/$', views.UserDetailView.as_view(), name='detail'),
 ]
--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
-def index(request):
-    return render(request, 'ledger/index.html')
+
+class IndexView(generic.ListView):
+    model = User
+    context_object_name = 'UserList'
+    template_name = 'ledger/index.html'
+
+class UserDetailView(generic.DetailView):
+    model = User
+    context_object_name = 'user'
+    template_name = 'ledger/user_detail.html'
+
+def add_user_page(request):
+    return render(request, 'ledger/add_user_page.html')
+
+def add_user(request):
+    input_name = request.POST.get('user_name')
+    new_user = User(user_name=input_name, founded=timezone.now())
+    new_user.save()
+    return HttpResponseRedirect(reverse('ledger:index'))
